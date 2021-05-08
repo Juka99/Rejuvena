98e0873930d9a7deab767289297f17bf578ef3cb
Aleksa Jukic <yukajunior007@gmail.com>
2021-05-08 15:01:28 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,6 +2,72 @@ const swup = new Swup();
 
 $(document).ready(function() {
 
+    let menuItems = $('.mainNavLink');
+
+    let sideMenuItems = $('.sideNavLink');
+
+    for(let el of sideMenuItems) {
+
+        el.classList.remove('activeLink');
+
+        el.addEventListener('click', changeActiveSide);
+
+    }
+
+    function changeActiveSide() {
+
+        for(let el of sideMenuItems) {
+
+            if(el.innerHTML != this.innerHTML) {
+
+                el.classList.remove('activeLink');
+
+            }
+
+            else {
+
+                this.classList.add('activeLink');
+
+            }
+
+        }
+
+    };
+
+    for(let el of menuItems) {
+
+        el.classList.remove('activeLink');
+
+        el.addEventListener('click', changeActiveMain);
+
+    }
+
+    function changeActiveMain() {
+
+        for(let el of menuItems) {
+
+            if(el.innerHTML != this.innerHTML) {
+
+                el.classList.remove('activeLink');
+
+            }
+
+            else {
+
+                this.classList.add('activeLink');
+
+            }
+
+        }
+
+    };
+
+    // Prikaz loadera u zavinsosti od vremena
+
+    loaderShow();
+
+    //////////////
+
     if(window.location.href.includes('index')) {
 
         $('.leftSideNav').removeClass('leftSideNavIndex');
@@ -15,6 +81,8 @@ $(document).ready(function() {
     }
 
     swup.on('contentReplaced', function() {
+
+        loaderShow();
 
         if(window.location.href.includes('index')) {
 
@@ -47,14 +115,6 @@ $(document).ready(function() {
     $(this).next().stop().slideToggle();
 
 });
-
-    // setTimeout(() => {
-
-    //     $('.spinner-container').addClass('spinnerHide');
-
-    //     $('body').addClass('overflowBody');
-
-    // }, 2000);
 
 
     $(".subMenuMainLink").on('mouseover', function() {
@@ -90,6 +150,16 @@ $(document).ready(function() {
         e.preventDefault();
 
         $(this).toggleClass('sideNavTriggerToggle');
+
+        $('.sideNavigationFirstBlock').toggleClass('sideNavigationFirstBlockToggle');
+
+        $('.sideNavigationSecondBlock').toggleClass('sideNavigationSecondBlockToggle');
+
+    });
+
+    $(".sideNavigationFirstBlock .sideNavLink").on('click', function() {
+
+        $('.sideNavTrigger').removeClass('sideNavTriggerToggle');
 
         $('.sideNavigationFirstBlock').toggleClass('sideNavigationFirstBlockToggle');
 
@@ -139,5 +209,83 @@ function scrollEvents() {
             $(".mainNavigation").removeClass('navChange');
 
         }
+
+}
+
+function loaderShow() {
+
+    let time = Math.round(new Date().getTime() / 1000 / 60);
+
+    if (localStorage) {
+
+        if (!localStorage.getItem('videoShowRejuvena')) {
+
+            setTimeout(() => {
+
+                $('.spinner-container').css({'opacity' : '0'});
+        
+                setTimeout(() => {
+        
+                    $('.spinner-container').css({'z-index' : '-1000'});
+        
+                }, 500);
+        
+                $('html').css('overflow-y', 'visible');
+
+                localStorage.setItem('videoShowRejuvena', time);
+        
+            }, 2000);
+
+        }
+
+        else {
+
+            let getTimeFromLocal = JSON.parse(localStorage.getItem('videoShowRejuvena'));
+
+            console.log(time - getTimeFromLocal, time, getTimeFromLocal)
+
+            if (time - getTimeFromLocal >= 20) {
+
+                setTimeout(() => {
+
+                    $('.spinner-container').css({'opacity' : '0'});
+            
+                    setTimeout(() => {
+            
+                        $('.spinner-container').css({'z-index' : '-1000'});
+            
+                    }, 500);
+            
+                    $('html').css('overflow-y', 'visible');
+
+                    localStorage.removeItem('videoShowRejuvena');
+            
+                }, 2000);
+
+            }
+
+            else {
+
+                $('.spinner-container').css({'display' : 'none'});
+
+                setTimeout(() => {
+            
+                    $('.spinner-container').css({'z-index' : '-1000'});
+        
+                }, 500);
+
+                $('html').css('overflow-y', 'visible');
+
+            }
+
+        }
+
+    }
+
+    else {
+
+        alert('Local Storage is not supported on your browser !');
+
+    }
 
 }
